ca5fd8c8c8a63f34ae898f5a1613349aa34901e9
Koquay <kkwilson27@hotmail.com>
2019-08-03 08:41:56 -0400

    authentication

--- backend/app/order/order.routes.js
+++ backend/app/order/order.routes.js
@@ -6,7 +6,7 @@ router.get('/prodForOrder/:orderNo', OrderController.getProductsForOrder);
 router.get('/search', UserController.authenticate, OrderController.searchOrder);
 router.get('/:status', UserController.authenticate, OrderController.getOrdersByStatus);
 router.get('/:orderNo/1', OrderController.getSearchedOrder);
-router.post('/:deleteItem/1', OrderController.deleteItem);
+router.post('/:deleteItem/1', UserController.authenticate, OrderController.deleteItem);
 router.post('/:status', UserController.authenticate, OrderController.setOrderStatus);
 router.post('/', OrderController.post);
 
